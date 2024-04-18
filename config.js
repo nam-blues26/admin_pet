ddb175eff1e9556cbd569184df9102959766dba9
Vu Hoai Nam <namhv2026@gmail.com>
2024-04-18 13:03:54 +0700

    up

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// export const API_BASE_URL = 'http://14.225.205.171:8099/api/v1';
-export const API_BASE_URL = 'http://localhost:8000/api/v1';
+export const API_BASE_URL = 'http://14.225.205.171:8099/api/v1';
+// export const API_BASE_URL = 'http://localhost:8000/api/v1';
 export const API_PRODUCT_IMAGE = API_BASE_URL+"/product/image/";
 export const API_POST_IMAGE = API_BASE_URL+"/post/image/";
